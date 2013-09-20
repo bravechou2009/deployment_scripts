a4af475d15c6031caf4273264f36e434b2304b53
Nathan Zimmerman <npzimmerman@gmail.com>
2013-09-20 15:21:00 -0400

    supervisor script

---
+++ supervisord_deploy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from __future__ import with_statement
+from fabric.api import (local, run, sudo, put, get, prompt, reboot, abort,
+     settings, env)
+from fabric.contrib.console import confirm
+import sys
+import fab_prompt
+
+def main():
+    """Deploy and configure virtualenv and its wrapper."""
+
+    sudo('pip install supervisor')
+    run("echo_supervisord_conf")
+    sudo("echo_supervisord_conf > /etc/supervisord.conf")
+    sys.exit()
+
+
+
+
+if __name__ == '__main__':
+   fab_prompt.prompt()
+   main()
