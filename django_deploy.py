51ada6634589a4bd5ba7a07e44ac971bf905a299
Nathan Zimmerman <npzimmerman@gmail.com>
2013-09-25 17:44:04 -0400

    django deployment now runs with uwsgi and nginx - conf file included - out of the box

--- django_deploy.py
+++ django_deploy.py
@@ -9,19 +9,9 @@ import fab_prompt
 import virtualenv_deploy
 import os
 
-def main():
-    """Deploy Django inside a virtual environment."""
-    env.shell = "/bin/bash -l -i -c"
-    django_project_name = raw_input('Django project name: ')
-    run('mkvirtualenv {0}-env'.format(django_project_name))
-    workon_virtual_env = 'workon {0}-env && '.format(django_project_name)
-    with cd('/home/{0}/.virtualenvs/{1}-env'.format(env.user, django_project_name)):
-        run(workon_virtual_env + 'pip install django south django-jenkins uwsgi')
-        run(workon_virtual_env + 'django-admin.py startproject {0}'.format(django_project_name))
-    django_project_dir = '/home/{0}/.virtualenvs/{1}-env/{1}'.format(env.user, django_project_name)
-    with cd(django_project_dir):
-        sudo('chmod +x manage.py')
 
+def uwsgi_setup(django_project_name, django_project_dir):
+    """set uwsgi up to serve django"""
     sudo('pip install uwsgi')
     uwsgi_ini_file = '/etc/uwsgi/vassals/{0}.ini'.format(django_project_name)
     sudo('mkdir -p /etc/uwsgi/vassals')
@@ -38,6 +28,31 @@ def main():
     sudo('echo "#stats = 127.0.0.1:9191" >> {0}'.format(uwsgi_ini_file))
     sudo('echo "wsgi-file = /home/{1}/.virtualenvs/{2}-env/{2}/{2}/uwsgi.py" >> {0}'.format(uwsgi_ini_file, env.user, django_project_name))
     sudo('echo "virtualenv = /home/{1}/.virtualenvs/{2}-env" >> {0}'.format(uwsgi_ini_file, env.user, django_project_name))
+
+
+def nginx_setup(django_project_name):
+    """set up nginx.conf to deal with uwsgi"""
+    nginx_conf_loc = '/usr/local/nginx/conf/nginx.conf'
+    sudo('rm {0}'.format(nginx_conf_loc))
+    sudo('touch {0}'.format(nginx_conf_loc))
+    for i, line in enumerate(open(os.path.join(os.getcwd(),'template.nginx.conf'),'r')):
+        sudo('echo "{0}" >> {1}'.format(line.format(env.user, django_project_name)[:-1], nginx_conf_loc))
+
+
+def main():
+    """Deploy Django inside a virtual environment."""
+    env.shell = "/bin/bash -l -i -c"
+    django_project_name = raw_input('Django project name: ')
+    run('mkvirtualenv {0}-env'.format(django_project_name))
+    workon_virtual_env = 'workon {0}-env && '.format(django_project_name)
+    with cd('/home/{0}/.virtualenvs/{1}-env'.format(env.user, django_project_name)):
+        run(workon_virtual_env + 'pip install django south django-jenkins uwsgi')
+        run(workon_virtual_env + 'django-admin.py startproject {0}'.format(django_project_name))
+    django_project_dir = '/home/{0}/.virtualenvs/{1}-env/{1}'.format(env.user, django_project_name)
+    with cd(django_project_dir):
+        sudo('chmod +x manage.py')
+    uwsgi_setup(django_project_name, django_project_dir)
+    nginx_setup(django_project_name)
 
 
 if __name__ == '__main__':
